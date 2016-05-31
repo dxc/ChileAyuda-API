706c0342cb1a820698c004c2a769b1c9a7f164f7
Francisco Madrid <fjmadrid@gmail.com>
2016-05-30 21:42:32 -0400

    removing config files from coverage report

--- run_tests.bat
+++ run_tests.bat
@@ -3,5 +3,5 @@
 SET SCRIPT_PATH=%~dp0
 SET DJANGO_SETTINGS_MODULE=chileayuda.settings.test
 
-coverage run --source='.' --branch --omit="manage.py,*/test*" --include=api.* %SCRIPT_PATH%/manage.py test --verbosity=2 %*
+coverage run --source='.' --branch --omit="manage.py,chileayuda/*,*/test*" --include=api.* %SCRIPT_PATH%/manage.py test --verbosity=2 %*
 coverage html
